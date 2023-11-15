9de91ab03a37f1a6ea5cb9f9b7be5321f175173e
kapselccc <kapselccc@wp.pl>
2023-11-15 14:13:21 +0100

    commit 2

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Training.DomainClasses
@@ -14,13 +15,33 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            return _petsInTheStore.OneAtTheTime();
+            return new ReadOnly<Pet>(_petsInTheStore);
         }
 
         public void Add(Pet newPet)
         {
             if (_petsInTheStore.Contains(newPet)) return;
             _petsInTheStore.Add(newPet);
+        }
+    }
+
+    public class ReadOnly<TItem> : IEnumerable<TItem>
+    {
+        private readonly IEnumerable<TItem> _pets;
+
+        public ReadOnly(IEnumerable<TItem> pets)
+        {
+            _pets = pets;
+        }
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            return _pets.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
