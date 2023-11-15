577fc6c8ab84b846ab4ad51ea62a28aef38bee19
manuxx <manus@pg.gda.pl>
2023-11-15 14:03:56 +0100

    zadanie - poczatek nowej skladni

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Training.DomainClasses
@@ -14,13 +15,33 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            return _petsInTheStore.OneAtATime();
+            return new ReadOnly(_petsInTheStore);
         }
 
         public void Add(Pet newPet)
         {
             if (_petsInTheStore.Contains(newPet)) return;
             _petsInTheStore.Add(newPet);
+        }
+    }
+
+    public class ReadOnly : IEnumerable<Pet>
+    {
+        private readonly IEnumerable<Pet> _pets;
+
+        public ReadOnly(IEnumerable<Pet> pets)
+        {
+            _pets = pets;
+        }
+
+        public IEnumerator<Pet> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
