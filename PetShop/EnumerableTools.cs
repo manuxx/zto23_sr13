8464ef9f12a220ef59207ac3d86c6fd28a1f8322
qaziok <72215548+qaziok@users.noreply.github.com>
2023-11-15 13:53:07 +0100

    184347_15-11_1

--- PetShop/EnumerableTools.cs
+++ PetShop/EnumerableTools.cs
@@ -1,13 +1,15 @@
 using System.Collections.Generic;
-using Training.DomainClasses;
 
-public static class EnumerableTools
+namespace Training.DomainClasses
 {
-    public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
+    public static class EnumerableTools
     {
-        foreach (var item in items)
+        public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
         {
-            yield return item;
+            foreach (var item in items)
+            {
+                yield return item;
+            }
         }
     }
 }
