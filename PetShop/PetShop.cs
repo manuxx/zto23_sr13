a7c8a1f98db3616a751143781afe8cf1d702a862
kbanach <jkkp12@outlook.com>
2023-11-15 14:12:03 +0100

    Rozwiazanie zad 1 z ReadOnly

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Training.DomainClasses
 {
@@ -14,13 +16,33 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            return _petsInTheStore.OneAtATime();
+            return new ReadOnly<Pet>(_petsInTheStore);
         }
 
         public void Add(Pet newPet)
         {
             if (_petsInTheStore.Contains(newPet)) return;
             _petsInTheStore.Add(newPet);
+        }
+    }
+
+    public class ReadOnly<Pet> : IEnumerable<Pet>
+    {
+        private readonly IEnumerable<Pet> _pets;
+
+        public ReadOnly(IEnumerable<Pet> pets)
+        {
+            _pets = pets;
+        }
+
+        public IEnumerator<Pet> GetEnumerator()
+        {
+            return _pets.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
