6818bb97f5ed9a7267d5d5fadfee73a6692ee968
kapselccc <kapselccc@wp.pl>
2023-11-22 15:03:07 +0100

    added new criteria

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -60,20 +60,4 @@ namespace Training.DomainClasses
             return pet => pet.species != species;
         }
     }
-
-    public class SexCriteria : Criteria<Pet>
-    {
-        private readonly Sex _petSex;
-
-        public SexCriteria(Sex petSex)
-        {
-            _petSex = petSex;
-        }
-
-
-        public bool isSatisfiedBy(Pet pet)
-        {
-            return _petSex == pet.sex;
-        }
-    }
 }
