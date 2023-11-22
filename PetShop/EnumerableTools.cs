566146d10855c7f464546623059891f92fa3db07
manuxx <manus@pg.gda.pl>
2023-11-22 14:41:05 +0100

    poczatek nowej skladni - refaktoring

--- PetShop/EnumerableTools.cs
+++ PetShop/EnumerableTools.cs
@@ -14,10 +14,34 @@ public static class EnumerableTools
 
     public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
     {
+        return items.ThatSatisfy(new AnonymousCriteria<TItem>(condition));
+    }
+    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
+    {
         foreach (var item in items)
         {
-            if (condition(item))
+            if (criteria.IsSatisfiedBy(item))
                 yield return item;
         }
     }
+}
+
+public class AnonymousCriteria<T> : Criteria<T>
+{
+    private readonly Predicate<T> _condition;
+
+    public AnonymousCriteria(Predicate<T> condition)
+    {
+        _condition = condition;
+    }
+
+    public bool IsSatisfiedBy(T item)
+    {
+        return _condition(item);
+    }
+}
+
+public interface Criteria<T>
+{
+    bool IsSatisfiedBy(T item);
 }
