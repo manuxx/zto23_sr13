f502129683109efcbb202c6d0313af821c5c1646
rlipinskiPL <rafal.lipinski.pl@proton.me>
2023-11-28 22:25:14 +0100

    Rozwiazanie

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace Training.DomainClasses
 {
@@ -25,6 +26,24 @@ namespace Training.DomainClasses
         public IEnumerable<Pet> AllPets()
         {
             return _petsInTheStore.OneAtATime();
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var result = new List<Pet>(_petsInTheStore);
+            result.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
+            return result;
+        }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Cat)
+                {
+                    yield return pet;
+                }
+            }
         }
 
         public void Add(Pet newPet)
